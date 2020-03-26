b08d1b7757850fa7f00a8e344fc0335f713e7be4
Rodrigo Chiaradia <rodrigochiaradia@hotmail.com>
2020-03-26 17:12:48 -0300

    a

--- semanaomnistack11/aulas/backend/index.js
+++ semanaomnistack11/aulas/backend/index.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
 app.get('/', (request, response) => {
     return response.json({
         Evento: 'Semana Omnistack 11.0',
         Aluno: 'Rodrigo Chiaradia',
-        Trabalho: 'teletrabalhoa'
+        Trabalho: 'teletrabalho'
     });
 });
 
