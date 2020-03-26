e6c9c0fee8435e0e809487b3414a91165c3548f5
Rodrigo Chiaradia <rodrigochiaradia@hotmail.com>
2020-03-26 17:56:09 -0300

    a

--- semanaomnistack11/aulas/backend/index.js
+++ semanaomnistack11/aulas/backend/index.js
@@ -9,12 +9,24 @@ const app = express();
 /**
  * Métodos HTTP:
  * 
- * GET: Buscar uma informação do back-end
+ * GET: Buscar/listar uma informação do back-end
  * POST: Criar uma informação no back-end
  * PUT: Alterar uma informação no back-end
  * DELETE: Deletar uma informação no back-end
  */
-app.get('/', (request, response) => {
+
+ /**
+  * Tipos de parâmetros
+  * 
+  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+  * Route Params: Parâmetros utlizados para identificar recursos
+  * Resquest Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+app.get('/users/:id', (request, response) => {
+    const params = request.params;
+
+    console.log(params);
+
     return response.json({
         Evento: 'Semana Omnistack 11.0',
         Aluno: 'Rodrigo Chiaradia',
