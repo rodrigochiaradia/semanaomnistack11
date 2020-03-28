6aa35cf40be3517245d6c02d8bea8541bd278607
Rodrigo Chiaradia <rodrigochiaradia@hotmail.com>
2020-03-28 19:49:57 -0300

    a

--- semanaomnistack11/aulas/backend/src/index.js
+++ semanaomnistack11/aulas/backend/src/index.js
@@ -1,6 +1,9 @@
 /*Aciona a dependência ("aplicação") 'express' na variável 'express'*/
 const express = require('express');
 
+//Dispara o cors - módulo que determina quem pode acessar a aplicação
+const cors = require('cors');
+
 /**
  * Importa as rotas do arquivo 'routes.js' para o index.js. No comando 'require', o parâmetro
  * (texto dentro das aspas simples) dentro dos parênteses é interpretado como um pacote,
@@ -14,6 +17,17 @@ const routes = require('./routes');
 /*Inicializa o app*/
 const app = express();
 
+/**
+ * Como ainda está em desenvolvimento, apenas o código a seguir basta.
+ app.use(cors());
+ * Este código permite a qualquer aplicação do frontend acessar o backend
+ * Quanto estiver em produção, dentro do parâmetro 'cors' pode ser enviada a opção 'origin'
+ * O código ficaria assim:
+ app.use(cors({
+    origin: 'http://meuapp.com'
+ }));
+ */
+app.use(cors());
 /**
  * Avisa ao nosso app que estaremos utilizando json para o corpo das requisições
  * Converte o json em um objeto do Javascript
