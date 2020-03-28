ea652c5cdd949bb11dad44739311f202accf0d0e
Rodrigo Chiaradia <rodrigochiaradia@hotmail.com>
2020-03-28 05:09:21 -0300

    a

--- semanaomnistack11/aulas/backend/src/routes.js
+++ semanaomnistack11/aulas/backend/src/routes.js
@@ -4,6 +4,68 @@ const express = require('express');
 /*Desacopla o "módulo de rotas" do express em uma nova variável*/
 const routes = express.Router();
 
+/**Executa o método do protocolo HTTP. 
+ * 
+ * * Métodos HTTP:
+*
+* GET: Buscar/listar uma informação do back-end
+* POST: Criar uma informação no back-end
+* PUT: Alterar uma informação no back-end
+* DELETE: Deletar uma informação no back-end
+*
+* Tipos de parâmetros
+*
+* Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+* aplicável ao método GET! Código a ser utilizado:
+
+const express = require('express');
+const app = express();
+app.get('/users', (request, response) => {
+   const params = request.query;
+   console.log(params);
+   return response.json({
+      Evento: 'Semana Omnistack 11.0',
+      Aluno: 'Rodrigo Chiaradia',
+      Trabalho: 'teletrabalho'
+   });
+});
+app.listen(3333);
+
+* Route Params: Parâmetros utlizados para identificar recursos
+* aplicável ao método GET! Código a ser utilizado:
+
+const express = require('express');
+const app = express();
+app.get('/users/:id', (request, response) => {
+   const params = request.params;
+   console.log(params);
+   return response.json({
+      Evento: 'Semana Omnistack 11.0',
+      Aluno: 'Rodrigo Chiaradia',
+      Trabalho: 'teletrabalho'
+   });
+});
+app.listen(3333);
+
+* Resquest Body: Corpo da requisição, utilizado para criar ou alterar recursos
+* aplicável ao método POST! Código a ser utilizado:
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post('/users', (request, response) => {
+   const body = request.body;
+   console.log(body);
+   return response.json({
+      Evento: 'Semana Omnistack 11.0',
+      Aluno: 'Rodrigo Chiaradia',
+      Trabalho: 'teletrabalho'
+   });
+});
+app.listen(3333);
+ 
+*/
+
 routes.post('/users', (request, response) => {
     const body = request.body;
 
@@ -12,7 +74,7 @@ routes.post('/users', (request, response) => {
     return response.json({
         Evento: 'Semana Omnistack 11.0',
         Aluno: 'Rodrigo Chiaradia',
-        Trabalho: 'teletrabalho'
+        Trabalho: 'online'
     });
 });
 
