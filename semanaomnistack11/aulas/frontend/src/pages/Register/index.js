40aec28bd60b2aba6a81750e644a2e6eb41035f6
Rodrigo Chiaradia <rodrigochiaradia@hotmail.com>
2020-03-29 11:46:29 -0300

    a

--- semanaomnistack11/aulas/frontend/src/pages/Register/index.js
+++ semanaomnistack11/aulas/frontend/src/pages/Register/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -15,14 +17,22 @@ export default function Register(){
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a 
                         encontrarem os casos de sua ONG.</p>
 
-                    <Link className="back-link" to="/register">
-                    < FiLogIn size={16} color="#e02041" />
+                    <Link className="back-link" to="/">
+                    < FiArrowLeft size={16} color="#e02041" />
                         Não tenho cadastro
                     </Link>
                 </section>
                 <form>
+                    <input placeholder="Nome da ONG" />
+                    <input type="email" placeholder="E-mail" />
+                    <input placeholder="WhatsApp" />
 
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style={{ width: 80 }} />
+                    </div>
 
+                    <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
